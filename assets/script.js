307f4b4423dec0c58cb3e456257c3649cb743f27
Mario <ruczkowski.m@gmail.com>
2023-02-06 23:29:59 +0000

    add one day weather for user input value

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,26 @@
-let APIKey = "732a88c6f9f8186453f47b435c9ba5f3";
+let button = document.querySelector('.button');
+let inputValue = document.querySelector('.inputValue');
+let name = document.querySelector('.name');
+let desc = document.querySelector('.desc');
+let temp = document.querySelector('.temp');
 
-let queryURL ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey; 
 
-fetch(queryURL)
-.then(response => response.json())
-.then(citiesFound => {
-    let firstCity = citiesFound[0];
-    console.log(firstCity.lat);
-    console.log(firstCity.lon);
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=" + APIKey`)
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=732a88c6f9f8186453f47b435c9ba5f3')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let nameValue = data.name;
+        let tempValue = data.main.temp;
+        let descValue = data.weather[0].description;
+        
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+        
+    })
 })
+    
 
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
+
