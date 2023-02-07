4f37e61a9f2f0130518d1d2c15b1e311986c4baf
Mario <ruczkowski.m@gmail.com>
2023-02-07 00:00:04 +0000

    style button and weather details container

--- assets/script.js
+++ assets/script.js
@@ -7,12 +7,12 @@ let temp = document.querySelector('.temp');
 
 
 button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=732a88c6f9f8186453f47b435c9ba5f3')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=732a88c6f9f8186453f47b435c9ba5f3')
     .then(response => response.json())
     .then(data => {
         console.log(data)
         let nameValue = data.name;
-        let tempValue = data.main.temp;
+        let tempValue = "Temp " + data.main.temp.toFixed(1) + "°C";
         let descValue = data.weather[0].description;
         
         name.innerHTML = nameValue;
